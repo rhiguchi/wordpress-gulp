ea13f1bfc3cd1fd09b7b2dafea650e2b76b1c236
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2016-08-26 12:10:01 +0900

    シンボリックリンクのパスを計算から求める

--- tasks/create-theme-symlink.js
+++ tasks/create-theme-symlink.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp')
 var fs = require('fs')
+var path = require('path')
 
 /**
  * テーマのビルドフォルダのシンボリックリンクを、 src/wp-content/themes 内に作ります
  */
 gulp.task('create-theme-symlink', function (cb) {
-  fs.symlink('../../../build/theme', 'src/wp-content/themes/beauty-wax', 'dir', cb)
+  // 出力先ディレクトリーからの相対パスを算出
+  var dest = 'src/wp-content/themes/beauty-wax'
+  var destDir = path.parse(dest).dir
+  var target = path.relative(destDir, 'build/theme');
+
+  fs.symlink(target, dest, 'dir', cb)
 })
